7f8ca7cc6808f653792826a19f46ff49c7d269c9
Antu <antu99gayen@gmail.com>
2023-06-01 16:52:23 +0530

    remove dotenv config from next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-// Initialize environment variables
-const dotenv = require("dotenv");
-
-dotenv.config();
-
 module.exports = nextConfig;
